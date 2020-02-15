0fc2981f3836ab6b6a483daf690a3ecb0ba2a8cc
Rene Fibus <renefib@googlemail.com>
2020-02-15 22:07:06 +0100

    Added last commands to publish the runbook and create the webhook

--- CreationOfAutomationAcc.ps1
+++ CreationOfAutomationAcc.ps1
@@ -19,3 +19,11 @@ New-AzAutomationAccount -ResourceGroupName $Config.resourceGroupName -Name $Conf
 New-AzAutomationRunbook -Name $Config.automationAccRunbookName -ResourceGroupName $Config.resourceGroupName -AutomationAccountName $Config.automationAccName -Type PowerShell -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 # Can you even deploy a runbook from powershell?
 Import-AzAutomationRunbook -AutomationAccountName $Config.automationAccName -ResourceGroupName $Config.resourceGroupName  -Path ".\DataCrawler.ps1" -Name $Config.automationAccRunbookName -Type PowerShell -Force
+#You need to publish the runbook to start it or create a webhook
+Publish-AzAutomationRunbook -AutomationAccountName $Config.automationAccName -ResourceGroupName $Config.resourceGroupName -Name $Config.automationAccRunbookName
+#For security purposes, the URL of the created webhook will only be viewable in the output of this command. No other commands will return the webhook URL. Make sure to copy down the webhook URL from this
+#command's output before closing your PowerShell session, and to store it securely
+New-AzAutomationWebhook -AutomationAccountName $Config.automationAccName -ResourceGroupName $Config.resourceGroupName -Name $Config.automationAccRunbookName -RunbookName $Config.automationAccRunbookName -IsEnabled $true -ExpiryTime "2020-12-12"
+
+#This webhook then needs to be configured in the environment variables of the function app. How to do this though without having it in the serverless.yml?
+ 
